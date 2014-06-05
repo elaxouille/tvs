165d7ceedeb2670099c732918beb411e56ec3090
Elaxis <contact@elaxis.fr>
2014-06-06 00:05:33 +0200

    Create tvs.py

---
+++ tvs.py
@@ -0,0 +1,15 @@
+#!/usr/bin env python
+# -*- coding: utf-8 -*-
+
+# Marge de mouvement non considéré
+marge = 50
+
+# Temps de réconfort (par tour)
+decrementationpeur = 1
+maxtouravantfinangoisse = 25
+
+# Espacement entre les pixels à calculer
+step = 4
+
+print "\033[0;34m[RESPIRE]\033[0m \033[0;32mDémarrage de la kinect\033[0m (CTRL-C pour quitter)";
+try:
